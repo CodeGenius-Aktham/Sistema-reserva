c960086c075d323e6b735b524ea830fe3e3efb6b
Camilo <camilomunozb203@gmail.com>
2025-07-26 23:10:06 -0500

    Modificacion de reservas.

--- reservas/backend/logic/reservas.py
+++ reservas/backend/logic/reservas.py
@@ -7,6 +7,7 @@ import psycopg2 # Importacion de la libreria que maneja la base de datos.
 app = Flask(__name__)
 CORS(app, origins="https://codegenius-aktham.github.io", supports_credentials=True)
 
+@app.after_request
 def apply_cors(response):
     response.headers["Access-Control-Allow-Origin"] = "https://codegenius-aktham.github.io"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
