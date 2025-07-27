e0dbc18d30b26739c309ecaffc07e25b174acf35
Camilo <camilomunozb203@gmail.com>
2025-07-26 23:03:18 -0500

    Modificacion de reservas.

--- reservas/backend/logic/reservas.py
+++ reservas/backend/logic/reservas.py
@@ -5,7 +5,7 @@ import psycopg2 # Importacion de la libreria que maneja la base de datos.
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://codegenius-aktham.github.io"}}, supports_credentials=True)
+CORS(app, origins="https://codegenius-aktham.github.io", supports_credentials=True)
 
 #Funcion que pasa la conexion con la base de datos.
 def conexion_db():
